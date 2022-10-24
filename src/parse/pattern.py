affebb83e2476c7d4afca5b87058cdc408c790bb
kerdl <clayshowtv@gmail.com>
2022-10-24 11:06:14 +0300

    new short name regex

--- src/parse/pattern.py
+++ src/parse/pattern.py
@@ -3,7 +3,7 @@ import re
 
 GROUP               = re.compile(r"(\d)([-]{0,1})([а-яёА-ЯЁ]{3})([-]{0,1})(\d{2})")
 NAME                = re.compile(r"[А-ЯЁ][а-яё]{1,}")
-SHORT_NAME          = re.compile(r"([А-ЯЁ][а-яё]{1,})(\s)([А-ЯЁ]{1}[.])([А-ЯЁ]{1}[.]){0,1}")
+SHORT_NAME          = re.compile(r"([А-ЯЁ][а-яё]{1,})(\s)([А-ЯЁ]{1}[.])([А-ЯЁ]{1}[.]{0,1}){0,1}")
 SPACE               = re.compile(r"\s")
 SPACE_NEWLINE       = re.compile(r"\n\s{1,}\n")
 DIGIT               = re.compile(r"\d")
