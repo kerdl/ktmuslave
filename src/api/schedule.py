d0f60c684905ce0bc4e1bd3ae82992ed882ba395
kerdl <clayshowtv@gmail.com>
2022-11-14 00:29:41 +0300

    idk why

--- src/api/schedule.py
+++ src/api/schedule.py
@@ -268,6 +268,9 @@ class ScheduleApi(Api):
                             await self.daily()
                             await self.weekly()
 
+                            if defs.ctx is None:
+                                continue
+
                             await defs.ctx.broadcast(notify)
                     except exceptions.ConnectionClosedError as e:
                         logger.info(e)
