cbbf00a7e41b2df1a7ae76923c104bba9ea3cf3c
kerdl <clayshowtv@gmail.com>
2023-06-27 13:55:57 +0600

    reset all now works

--- src/__init__.py
+++ src/__init__.py
@@ -259,7 +259,7 @@ class Defs:
             middlewares.r.assign()
         
         if init_handlers:
-            from src.svc.common.bps import settings, init, zoom, hub, reset
+            from src.svc.common.bps import reset, settings, init, zoom, hub
         
         from src.svc.common import Ctx
 
